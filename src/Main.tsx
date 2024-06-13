393044849d268241ab9a844412fc6d30b60c7e5a
mihnea112 <ayanna.mihnea@yahoo.com>
2024-06-13 22:25:21 +0300

    visual updates

--- src/Main.tsx
+++ src/Main.tsx
@@ -6,7 +6,6 @@ export default function Main() {
     <>
       <CarouselHero></CarouselHero>
       <Content></Content>
-      <Contact></Contact>
     </>
   );
 }
--- src/home/Carousel.tsx
+++ src/home/Carousel.tsx
@@ -10,32 +10,27 @@ export default function CarouselHero() {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Transformare digitală în industria auto</h3>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={500}>
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             src="/img/service.jpg"
-            alt="First slide"
+            alt="Second slide"
           />
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Eficiență și transparență</h3>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
             src="/img/service.jpg"
-            alt="First slide"
+            alt="Third slide"
           />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>Inovație și competitivitate</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
